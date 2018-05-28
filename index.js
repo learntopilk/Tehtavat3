e9d3e86ec6b9b64c035fef59e47d61462af49e59
learntopilk <jonsterivilja@gmail.com>
2018-05-28 19:59:34 +0300

    Tehtävä 3.16-3.18 tehty!
    Tietokannasta voi nyt poistaa henkilötietoja käyttöliittymän kautta. Henkilön tietoja voi myös päivittää, ja /info sekä /api/persons/:id tarjoavat asiamukaiset tiedot lukijalle.

--- index.js
+++ index.js
@@ -28,9 +28,12 @@ const parametersOK = (person) => {
 
 app.get("/info", (req, res) => {
   Contact
-   .find()
-   .then(res = res.send(`<div>palvelimella on ${res.length} yhteystietoa.</div>`))
-  //res.send(`<div>Palvelimella on ${persons['persons'].length} yhteystietoa.</div><div>${new Date()}</div>`)
+    .find()
+    .then(result => res.send(`<div>palvelimella on ${result.length} yhteystietoa.</div>`))
+    .catch(err => {
+      console.log("Error while getting all contacts for contact list length: " + err)
+      response.status(404).end()
+    })
 })
 
 app.get("/api/persons", (req, res) => {
@@ -40,6 +43,10 @@ app.get("/api/persons", (req, res) => {
       let p = persons.map(person => Contact.format(person))
       console.log("p: ", p)
       res.json(p)
+    })
+    .catch(err => {
+      console.log("Error while getting all contacts: " + err)
+      response.status(404).end()
     })
 
 })
@@ -54,6 +61,7 @@ app.get("/api/persons/:id", (req, res) => {
       console.log(pers[0])
       res.status(200).json(Contact.format(pers[0]))
     })
+    .catch(err => console.log("Error while getting contact by Id: " + err))
 })
 
 app.post("/api/persons/", (req, res) => {
@@ -82,36 +90,60 @@ app.post("/api/persons/", (req, res) => {
               console.log(result)
               res.status(200).json(Contact.format(result))
             })
+            .catch(err => {
+              console.log("Error while saving contact: ", err)
+              response.status(404).end()
+            })
         }
+      })
+      .catch(err => {
+        console.log("Error while getting getting contacts for saving: " + err)
+        response.status(404).end()
       })
   } else {
     res.status(400).json({ 'error': 'Invalid parameters' })
   }
 })
 
-
+// TODO: implement deleting
 app.delete("/api/persons/:id", (req, res) => {
-  let found = false;
-  let newList = []
-
-  persons['persons'].map(person => {
-    if (person.id !== Number(req.params.id)) {
-      newList.push(person)
-    } else {
-      found = true
-      return false
-    }
-  })
-
-  if (found) {
-    persons['persons'] = newList;
-    console.log("Persons: ", persons['persons'])
-    res.status(200).end()
-  } else {
-    res.status(404).end()
-  }
+  Contact
+    .findByIdAndRemove({_id: req.params.id})
+    .then(result => {
+      console.log(result)
+      res.status(204).end()
+    })
+    .catch(err => {
+      res.status(404).json({'error': 'Something went wrong when deleting'})
+    })
 })
 
+app.put("/api/persons/:id", (req, res) => {
+  
+  const c = {
+    name: req.body.name,
+    number: req.body.number
+  }
+  console.log("c: ", c)
+  Contact
+    .findByIdAndUpdate({_id: req.params.id}, c, {new: true})
+    .then(updated => {
+      console.log(updated)
+      res.json(Contact.format(updated))
+    })
+    .catch(err => {
+      res.status(400).json({error: "Probably a malformatted id!"})
+      console.log(err)
+    })
+})
+
+app.get("/", (req, res) => {
+  Contact
+  .find({})
+  .then(result => {
+    res.json(result.map(r => Contact.format(r)))
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
